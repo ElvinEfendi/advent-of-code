adfd6ebe8005391cd2bea6e7d0986a4894f1be13
Elvin Efendi <elvin.efendiyev@gmail.com>
2023-12-02 13:00:44 -0500

    day 1 part 2

--- 2023/1/src/main.rs
+++ 2023/1/src/main.rs
@@ -1,10 +1,28 @@
 use std::collections::HashMap;
 
 fn main() {
+    let digit_words: HashMap<&str, &str> = [
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ].iter().cloned().collect();
+
     let sum = std::fs::read_to_string("input.txt")
         .unwrap()
         .lines()
         .map(|line| {
+            let mut line = line.to_string();
+
+            for (word, digit) in digit_words.iter() {
+                line = line.replace(word, format!("{}{}{}", word, digit, word).as_str());
+            }
+
             let digits = line.chars()
                 .filter(|c| c.is_digit(10))
                 .map(|c| c.to_digit(10).unwrap())
