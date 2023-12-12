80d7cbf99a9ea7d82e06ac394ea71e3c4b20ec74
Elvin Efendi <elvin.efendiyev@gmail.com>
2023-12-11 20:12:00 -0500

    day 5 part 2 efficient with range search

--- 2023/5/src/almanac.rs
+++ 2023/5/src/almanac.rs
@@ -1,58 +1,122 @@
-use std::collections::HashMap;
-use rayon::prelude::*;
-
-#[derive(Debug)]
-struct MapEntry {
-    source_range_start: u32,
-    destination_range_start: u32,
-    range_length: u32,
+#[derive(Debug, Clone, PartialEq)]
+struct Range {
+    start: i64,
+    end: i64,
 }
 
-struct SeedEntry {
-    range_start: u32,
-    range_length: u32,
+#[derive(Debug)]
+struct Mapping {
+    source: Range,
+    delta: i64,
+}
+
+#[derive(Debug)]
+struct Map {
+    mappings: Vec<Mapping>,
+}
+
+impl Map {
+    fn apply(&self, range: &Range) -> Vec<Range> {
+        let mut result = Vec::new();
+        let mut overlap_source_ranges = Vec::new();
+
+        for mapping in &self.mappings {
+            let overlap_start = mapping.source.start.max(range.start);
+            let overlap_end = mapping.source.end.min(range.end);
+            if overlap_start <= overlap_end {
+                overlap_source_ranges.push(Range {
+                    start: overlap_start,
+                    end: overlap_end,
+                });
+                result.push(Range {
+                    start: overlap_start + mapping.delta,
+                    end: overlap_end + mapping.delta,
+                });
+            }
+        }
+
+        if overlap_source_ranges.is_empty() {
+            result.push(range.clone());
+        } else {
+            overlap_source_ranges.sort_unstable_by_key(|r| r.start);
+            if overlap_source_ranges.len() == 1 {
+                if overlap_source_ranges[0].start > range.start {
+                    result.push(Range {
+                        start: range.start,
+                        end: overlap_source_ranges[0].start - 1,
+                    });
+                }
+                if overlap_source_ranges[0].end < range.end {
+                    result.push(Range {
+                        start: overlap_source_ranges[0].end + 1,
+                        end: range.end,
+                    });
+                }
+            } else {
+                for overlap_source_range in overlap_source_ranges.windows(2) {
+                    if overlap_source_range[0].start > range.start {
+                        result.push(Range {
+                            start: range.start,
+                            end: overlap_source_range[0].start - 1,
+                        });
+                    }
+                    if overlap_source_range[0].end + 1 < overlap_source_range[1].start {
+                        result.push(Range {
+                            start: overlap_source_range[0].end + 1,
+                            end: overlap_source_range[1].start - 1,
+                        });
+                    }
+                }
+            }
+        }
+
+        result
+    }
 }
 
 pub struct Almanac {
-    seeds: Vec<SeedEntry>,
-    maps: HashMap<String, Vec<MapEntry>>,
+    seeds: Vec<Range>,
+    maps: Vec<Map>,
 }
 
 impl From<&str> for Almanac {
     fn from(s: &str) -> Self {
-        let mut seeds = Vec::new();
-        let mut maps = HashMap::new();
-
         let mut lines = s.lines();
 
+        let mut seeds = Vec::new();
         let seeds_line = lines.next().unwrap().split_once(": ").unwrap().1;
         for pair in seeds_line.split_whitespace().collect::<Vec<&str>>().chunks(2) {
-            seeds.push(SeedEntry {
-                range_start: pair[0].parse::<u32>().unwrap(),
-                range_length: pair[1].parse::<u32>().unwrap(),
-            });
+            let start = pair[0].parse::<i64>().unwrap();
+            let end = start + pair[1].parse::<i64>().unwrap() - 1;
+            seeds.push(Range { start, end });
         }
 
-        let mut map_name = "seed-to-soil";
+        let mut maps = Vec::new();
+        let mut mappings = Vec::new();
         while let Some(line) = lines.next() {
-            if line.contains("map:") {
-                map_name = line.split_whitespace().next().unwrap();
+            if line.contains("map") {
+                if !mappings.is_empty() {
+                    maps.push(Map { mappings });
+                    mappings = Vec::new();
+                }
             } else if !line.trim().is_empty() {
                 let mut entries = line.split_whitespace();
-                let destination_range_start = entries.next().unwrap().parse::<u32>().unwrap();
-                let source_range_start = entries.next().unwrap().parse::<u32>().unwrap();
-                let range_length = entries.next().unwrap().parse::<u32>().unwrap();
+                let destination_range_start = entries.next().unwrap().parse::<i64>().unwrap();
+                let source_range_start = entries.next().unwrap().parse::<i64>().unwrap();
+                let range_length = entries.next().unwrap().parse::<i64>().unwrap();
 
-                let map_entry = MapEntry {
-                    source_range_start,
-                    destination_range_start,
-                    range_length,
+                let mapping = Mapping {
+                    source: Range {
+                        start: source_range_start,
+                        end: source_range_start + range_length - 1,
+                    },
+                    delta: destination_range_start - source_range_start,
                 };
-
-                maps.entry(map_name.to_string())
-                    .or_insert_with(Vec::new)
-                    .push(map_entry);
+                mappings.push(mapping);
             }
+        }
+        if !mappings.is_empty() {
+            maps.push(Map { mappings });
         }
 
         Almanac {
@@ -63,46 +127,28 @@ impl From<&str> for Almanac {
 }
 
 impl Almanac {
-    fn find_in_map(&self, map_name: &str, value: u32) -> u32 {
-        let map = self.maps.get(map_name).unwrap();
+    pub fn find_lowest_location(&self) -> i64 {
+        let mut lowest_location = i64::MAX;
 
-        for entry in map {
-            if value < entry.source_range_start {
-                continue;
+        for seed in &self.seeds {
+            let mut ranges = vec![seed.clone()];
+
+            for map in &self.maps {
+                let mut output_ranges = Vec::new();
+                for range in &ranges {
+                    output_ranges.append(&mut map.apply(range));
+                }
+                ranges = output_ranges;
             }
 
-            let delta = value - entry.source_range_start;
-            if delta < entry.range_length {
-                return entry.destination_range_start + delta;
+            for range in ranges {
+                if range.start < lowest_location {
+                    lowest_location = range.start;
+                }
             }
         }
 
-        value
-    }
-
-    pub fn find_lowest_location(&self) -> u32 {
-        self.seeds.par_iter()
-            .map(|seed| {
-                let mut lowest_location = u32::MAX;
-
-                for i in 0..seed.range_length {
-                    let soil = self.find_in_map("seed-to-soil", seed.range_start + i);
-                    let fertilizer = self.find_in_map("soil-to-fertilizer", soil);
-                    let water = self.find_in_map("fertilizer-to-water", fertilizer);
-                    let light = self.find_in_map("water-to-light", water);
-                    let temperature = self.find_in_map("light-to-temperature", light);
-                    let humidity = self.find_in_map("temperature-to-humidity", temperature);
-                    let location = self.find_in_map("humidity-to-location", humidity);
-
-                    if location < lowest_location {
-                        lowest_location = location;
-                    }
-                }
-
-                lowest_location
-            })
-            .min()
-            .unwrap()
+        lowest_location
     }
 }
 
@@ -110,6 +156,79 @@ impl Almanac {
 
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_map_apply() {
+        // full overlap
+        let map = Map {
+            mappings: vec![
+                Mapping {
+                    source: Range { start: 5, end: 10 },
+                    delta: 2,
+                },
+            ],
+        };
+        let range = Range { start: 5, end: 10 };
+        let result = map.apply(&range);
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].start, 7);
+        assert_eq!(result[0].end, 12);
+
+        // partial overlap
+        let map = Map {
+            mappings: vec![
+                Mapping {
+                    source: Range { start: 5, end: 10 },
+                    delta: 2,
+                },
+            ],
+        };
+        let range = Range { start: 7, end: 12 };
+        let result = map.apply(&range);
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0].start, 9);
+        assert_eq!(result[0].end, 12);
+        assert_eq!(result[1].start, 11);
+        assert_eq!(result[1].end, 12);
+
+        // partial overlap across multiple mappings
+        let map = Map {
+            mappings: vec![
+                Mapping {
+                    source: Range { start: 5, end: 10 },
+                    delta: 2,
+                },
+                Mapping {
+                    source: Range { start: 15, end: 20 },
+                    delta: 3,
+                },
+            ],
+        };
+        let range = Range { start: 7, end: 18 };
+        let result = map.apply(&range);
+        assert_eq!(result.len(), 3);
+        assert_eq!(result[0].start, 9);
+        assert_eq!(result[0].end, 12);
+        assert_eq!(result[1].start, 18);
+        assert_eq!(result[1].end, 21);
+        assert_eq!(result[2].start, 11);
+        assert_eq!(result[2].end, 14);
+
+        // no overlap
+        let map = Map {
+            mappings: vec![
+                Mapping {
+                    source: Range { start: 5, end: 10 },
+                    delta: 2,
+                },
+            ],
+        };
+        let range = Range { start: 1, end: 4 };
+        let result = map.apply(&range);
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].start, 1);
+        assert_eq!(result[0].end, 4);
+    }
 
     #[test]
     fn test_almanac_from_str() {
@@ -127,18 +246,15 @@ soil-to-fertilizer map:
         let almanac = Almanac::from(input);
 
         assert_eq!(almanac.seeds.len(), 2);
-        assert_eq!(almanac.seeds[0].range_start, 79);
-        assert_eq!(almanac.seeds[0].range_length, 14);
-        assert_eq!(almanac.seeds[1].range_start, 55);
-        assert_eq!(almanac.seeds[1].range_length, 13);
-
+        assert_eq!(almanac.seeds[0].start, 79);
+        assert_eq!(almanac.seeds[0].end, 92);
+        assert_eq!(almanac.seeds[1].start, 55);
+        assert_eq!(almanac.seeds[1].end, 67);
         assert_eq!(almanac.maps.len(), 2);
-        assert_eq!(almanac.maps.get("seed-to-soil").unwrap().len(), 2);
-        assert_eq!(almanac.maps.get("soil-to-fertilizer").unwrap().len(), 3);
-
-        assert_eq!(almanac.maps.get("seed-to-soil").unwrap()[0].source_range_start, 98);
-        assert_eq!(almanac.maps.get("seed-to-soil").unwrap()[0].destination_range_start, 50);
-        assert_eq!(almanac.maps.get("seed-to-soil").unwrap()[0].range_length, 2);
+        assert_eq!(almanac.maps[0].mappings.len(), 2);
+        assert_eq!(almanac.maps[0].mappings[0].source.start, 98);
+        assert_eq!(almanac.maps[0].mappings[0].source.end, 99);
+        assert_eq!(almanac.maps[0].mappings[0].delta, -48);
     }
 
     #[test]
